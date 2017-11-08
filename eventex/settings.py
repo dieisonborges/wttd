252fd506ccf4d1202a037ecdc5b5c662fce5ee3d
Dieison S. Borges <dieisoncomix@gmail.com>
2017-11-08 01:20:27 -0200

    Remove django_extensions

--- eventex/settings.py
+++ eventex/settings.py
@@ -44,8 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # APPS de terceiros
-    'django_extensions',
+    #'django_extensions',
     'eventex.core',
     'eventex.subscriptions',
 )
