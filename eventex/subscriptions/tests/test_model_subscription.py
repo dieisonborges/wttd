a41d7e86bbbf9d5a4316f990ff1e1997c71da27a
Dieison S. Borges <dieisoncomix@gmail.com>
2017-11-08 02:08:17 -0200

    Persiste a migracao

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+
+from datetime import datetime
+
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Dieison Borges',
+            cpf='12345678901',
+            email='dieisoncomix@gmail.com',
+            phone='41-995814222'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto created_at attr."""
+        self.assertIsInstance(self.obj.created_at, datetime)
